a7d33f4a276990f698e0af080cef48d77ed28c44
Chen Huang <chen.huang678@gmail.com>
2025-04-23 15:12:24 +0100

    Update

---
+++ src/components/About/About.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import SkewBg from '@common/SkewBg';
+import PageHeader from '@common/PageHeader';
+import Flex from '@common/Flex';
+
+import Quote from './Quote';
+import Avatar from './Avatar';
+
+import { AboutWrapper, AboutInfo } from './About.style';
+
+const About = () => {
+  return (
+    <AboutWrapper id="about">
+      <PageHeader>About Me</PageHeader>
+      <SkewBg />
+      <AboutInfo>
+        <div>
+          <Avatar src="hfest_avatar_2.jpg" />
+        </div>
+        <p>
+        Hi, I'm Chen Huang, an MSc student in Physics at Imperial College London and a remote research intern at the 
+        <a className="about__link" href="http://en.baqis.ac.cn/research/groups/?cid=814">Beijing Academy of Quantum Information Sciences (BAQIS)</a>.
+          <br />
+          <br />
+          I am passionate about advancing quantum computing through both theoretical research and experimental realization. My academic journey has equipped me with a strong foundation in physics and hands-on experience with a variety of quantum systems, including neutral atoms, trapped ions, and semiconductor qubits.
+          <br />
+          <br />
+          I also work extensively with quantum circuits. At BAQIS, my research focuses on developing efficient quantum compilation strategies that help bridge the gap between hardware capabilities and practical, scalable quantum computing.
+        </p>
+      </AboutInfo>
+    </AboutWrapper>
+  );
+};
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import svgRect from '@src/static/home_rect.svg';
+
+import { HeroCard } from './HeroCard';
+import { HomeWrapper, Intro } from './Home.style';
+
+import IconLink from '@common/IconLink';
+import PageHeader from '@common/PageHeader';
+import Flex from '@common/Flex';
+import Button from '@common/Button';
+
+import { Card, CardIcon, CardText, CardTitle } from '@common/Card';
+
+const ThingsILove = () => (
+  <Flex justify="space-between" align="center">
+    <Card>
+      <CardIcon>
+        <FontAwesomeIcon icon="code" />
+      </CardIcon>
+      <CardTitle>Physics</CardTitle>
+      <CardText>
+        I am majoring in Physics and all about quantum.
+      </CardText>
+    </Card>
+
+    <Card>
+      <CardIcon>
+        <FontAwesomeIcon icon={['fab', 'js']} />
+      </CardIcon>
+      <CardTitle>Quantum Computing</CardTitle>
+      <CardText>
+        My research focus on quantum computing.
+      </CardText>
+    </Card>
+
+    <Card>
+      <CardIcon>
+        <FontAwesomeIcon icon="paint-brush" />
+      </CardIcon>
+      <CardTitle>Coding</CardTitle>
+      <CardText>
+        I love coding.
+      </CardText>
+    </Card>
+  </Flex>
+);
+
+const Home = () => {
+  return (
+    <HomeWrapper id="home">
+      <img className="svg-rect" src={svgRect} alt=""></img>
+
+      <Intro>
+        {/* <Parallax y={[50, -50]} className="home__text"> */}
+        <div className="home__text">
+          <p>Hello, I'm</p>
+          <h1>CHEN HUANG</h1>
+          <p className="adjust">EXPLORING THE QUANTUM WORLD</p>
+
+          <div className="home__CTA">
+            <Button className="cta" as="a" href="#">
+              Download Resume
+            </Button>
+
+            <div className="home__social">
+              <IconLink
+                label="github"
+                icon={['fab', 'github']}
+                href="//github.com/chenx820"
+              />
+              <IconLink
+                label="email"
+                icon={['fab', 'email']}
+                href="mailto:chen.huang23@imperial.ac.uk"
+              />
+              <IconLink
+                label="linkedin"
+                icon={['fab', 'linkedin']}
+                href="//linkedin.com/in/chen-huang-820x/"
+              />
+            </div>
+          </div>
+        </div>
+        {/* </Parallax> */}
+        <HeroCard />
+      </Intro>
+
+      {/* Things I LOVE */}
+      <PageHeader style={{ marginBottom: 30 }}>
+        Things I love <i className="fas fa-heart" />
+      </PageHeader>
+      <ThingsILove />
+    </HomeWrapper>
+  );
+};
+
+export default Home;
