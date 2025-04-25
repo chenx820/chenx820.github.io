f823e4440cd2c1627b929ad7571840e52e3bd8ef
Chen Huang <chen.huang678@gmail.com>
2025-04-25 16:57:02 +0100

    Update

--- src/components/Home/HeroCard.js
+++ src/components/Home/HeroCard.js
@@ -30,90 +30,142 @@ function copyToClipboard(str) {
   document.body.removeChild(el);
 }
 
-export const CodeCard = () => {
-  const [text, setText] = useState(`new Date().getFullYear() - 2000;`);
-  const age = new Date().getFullYear() - 2000;
-  // trimed down polyfill of String.repeat
-  const changeText = () => {
-    let space = repeatString(" ", 54);
-    setText(age + ";" + space);
-  };
+export const CodeCard = ({ codeLines }) => {
   return (
     <CodeCardWrapper>
-      <pre>
-        1&nbsp;&nbsp;class <b>Physicist</b>:
-      </pre>
-      <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;def __init__(self):</pre>
-      <pre>
-        3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self.name =
-        "<b>Chen Huang</b>"
-      </pre>
-      <pre>
-        4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self.traits
-        = ["
-        <b>QUANTUM COMPUTING</b>"]
-      </pre>
-      <pre>
-        5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self.email
-        = "chen.huang23@imperial.ac.uk"
-      </pre>
-      <pre>
-        6&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;self.education
-        = {"{"}
-      </pre>
-      <pre>
-        7&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"MSc":
-        "Imperial College London",
-      </pre>
-      <pre>
-        8&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"BSc":
-        "Huazhong University of Science and Technology"
-      </pre>
-      <pre>
-        9&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{"}"}
-      </pre>
+      {codeLines.map((line, index) => (
+        <pre key={index}>{line}</pre>
+      ))}
     </CodeCardWrapper>
   );
 };
 
-const ColorBox = ({ color }) => {
+const ColorBox = ({ color, changeText }) => {
   const tooltipRef = useRef();
   useEffect(() => {
     return tooltipRef.current.addEventListener("animationend", () => {
       tooltipRef.current.classList.remove("tooltip-animate");
     });
   });
-  const copy = () => {
-    copyToClipboard(color);
-    tooltipRef.current.classList.add("tooltip-animate");
+  const onClick = () => {
+    changeText();
+    tooltipRef.current?.classList.add("tooltip-animate");
   };
 
   return (
     <ColorBoxWrapper
       ref={tooltipRef}
-      onClick={copy}
+      onClick={onClick}
       style={{ background: color }}
     />
   );
 };
 
-export const ColorPalette = withTheme(({ theme }) => {
+export const ColorPalette = withTheme(({ changeText, defaultCode }) => {
+  const colors = [
+    {
+      color: "#082b41",
+      message: defaultCode,
+    },
+    {
+      color: "#105286",
+      message: [
+        `10         self.projects = {`,
+        `11             \"Imperial\": [`,
+        `12                 \"Charge noise in semiconductor qubits\",`,
+        `13                 \"Quantum computing stack optimization\"`,
+        `14             ],`,
+        `15             \"BAQIS\": [`,
+        `16                 \"Quantum compilation with neutral atoms\",`,
+        `17                 \"Quantum error correction in neutral atoms\"`,
+        `18             ],`,
+        `19             \"Baidu\": [`,
+        `20                 \"Automatic calibration for trapped-ion systems\"`,
+        `21             ]`,
+        `22         }`,
+      ],
+    },
+    {
+      color: "#F1F2F4",
+      message: [
+        `23         self.skills = {`,
+        `24             \"Programming\": [\"Python\", \"MATLAB\", \"LaTeX\"],`,
+        `25             \"Quantum Computing\": [\"Qiskit\", \"QuTiP\"],`,
+        `26             \"Experimental\": [`,
+        `27                   \"Dilution refrigerators\",`,
+        `28                   "Semiconductor device\"`,
+        `29             ],`,
+        `30             \"Languages\": {`,
+        `31                 \"Chinese\": \"Native\",`,
+        `32                 \"English\": \"Fluent\"`,
+        `33             }`,
+        `34         }`,
+      ],
+    },
+    {
+      color: "#FDB338",
+      message: [
+        `35         self.awards_and_honors = {`,
+        `36             \"HUST\": [`,
+        `37                 \"China National Scholarship\",`,
+        `38                 \"Outstanding UG in Term of Academic Performance\",`,
+        `39                 \"Outstanding Graduate\"`,
+        `40             ],`,
+        `41             \"Chinese Academy of Sciences\": [`,
+        `42                 \"Yan Ji-Ci Scholarship\",`,
+        `43                 \"UCAS Scholarship\"`,
+        `44             ],`,
+        `45             \"Baidu Research\": [\"Outstanding Intern\"]`,
+        `46         }`,
+      ],
+    },
+    {
+      color: "#171D1D",
+      message: [
+        `47        self.leadership = {`,
+        `48            \"HerSTEM\": \"Co-founder\",`,
+        `49            \"IBPE\": \"President\"`,
+        `50        }`,
+      ],
+    },
+  ];
+
   return (
     <ColorPaletteWrapper>
-      <ColorBox color={theme.primaryColor} />
-      <ColorBox color={theme.secondaryColor} />
-      <ColorBox color={theme.gradient} />
-      <ColorBox color={theme.primaryBlack} />
-      <ColorBox color={theme.accentColor} />
+      {colors.map(({ color, message }) => (
+        <ColorBox
+          key={color}
+          color={color}
+          changeText={() => changeText(message)}
+        />
+      ))}
     </ColorPaletteWrapper>
   );
 });
 
 export const HeroCard = () => {
+  const defaultCode = [
+    `1  class Physicist:`,
+    `2      def __init__(self):`,
+    `3          self.name = \"Chen Huang\"`,
+    `4          self.traits = [\"quantum computing\"]`,
+    `5          self.email = \"chen.huang23@imperial.ac.uk\"`,
+    `6          self.education = {`,
+    `7              \"MSc\": \"Imperial College London\",`,
+    `8              \"BSc\": \"Huazhong University of Science and Technology\"`,
+    `9          }`,
+  ];
+
+  const [codeLines, setCodeLines] = useState(defaultCode);
+
+  const changeText = (lines) => {
+    setCodeLines(lines);
+  };
+
   return (
     <HeroCardWrapper>
-      <CodeCard />
-      <ColorPalette />
+      <CodeCard codeLines={codeLines} />
+      <ColorPalette changeText={changeText} defaultCode={defaultCode} />
     </HeroCardWrapper>
   );
 };
