d030fa5a9a7774f34b602e3d99e544ac5a954b7c
Chen Huang <chen.huang678@gmail.com>
2025-09-05 13:47:24 +0100

    Auto commit before deployment

--- src/components/Layout/Navbar/NavDesktop.js
+++ src/components/Layout/Navbar/NavDesktop.js
@@ -35,12 +35,12 @@ const NavDesktop = () => {
           <NavItem>
             <Link to="/gallery">{t("nav.gallery")}</Link>
           </NavItem>
-          <NavItem>
-            <ToggleSwitch />
-          </NavItem>
           {/* <NavItem>
-            <LanguageSwitch />
+            <ToggleSwitch />
           </NavItem> */}
+          <NavItem>
+            <LanguageSwitch />
+          </NavItem>
         </NavItems>
       </nav>
     </>
