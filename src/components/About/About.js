f854e62d669c51c99b60e8053c9ce2d8aafa21dc
Chen Huang <chen.huang678@gmail.com>
2025-09-13 16:33:11 +0800

    Auto commit before deployment

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -19,30 +19,28 @@ const About = () => {
           <Avatar src="avatar.png" />
         </div>
         <p>
-          <Trans>
-            Hi, I'm Chen, an incoming PhD student in Computer Science and
-            Engineering at{" "}
-            <a
-              className="about__link"
-              target="__blank"
-              href="https://www.innovationadvancedlab.com/home"
-            >
-              The Chinese University of Hong Kong (CUHK)
-            </a>
-            .
-            <br />
-            <br />
-            I am passionate about advancing quantum computing through both
-            theoretical research and experimental realization. My academic
-            journey has equipped me with a strong foundation in physics and
-            hands-on experience with a variety of quantum systems, including
-            neutral atoms, trapped ions, and semiconductor qubits.
-            <br />
-            <br />I also work with quantum circuits. At BAQIS, my research
-            focuses on developing efficient quantum compilation strategies that
-            help bridge the gap between hardware capabilities and practical,
-            scalable quantum computing.
-          </Trans>
+          Hi, I'm Chen, an incoming PhD student in Computer Science and
+          Engineering at{" "}
+          <a
+            className="about__link"
+            target="__blank"
+            href="https://www.innovationadvancedlab.com/home"
+          >
+            <strong>The Chinese University of Hong Kong (CUHK)</strong>
+          </a>
+          .
+          <br />
+          <br />
+          I am passionate about advancing quantum computing through both
+          theoretical research and experimental realization. My academic journey
+          has equipped me with a strong foundation in physics and hands-on
+          experience with a variety of quantum systems, including neutral atoms,
+          trapped ions, and semiconductor qubits.
+          <br />
+          <br />I also work with quantum circuits. At BAQIS, my research focuses
+          on developing efficient quantum compilation strategies that help
+          bridge the gap between hardware capabilities and practical, scalable
+          quantum computing.
         </p>
       </AboutInfo>
 
